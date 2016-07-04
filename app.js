6c03db5d579f2f2b6bf8608121691a09264546d1
Big Ethan <e@bigethan.com>
2016-07-04 10:09:34 +0100

    updating packages

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
